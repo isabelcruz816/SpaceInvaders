4ed81cb04af1c60d8f54f0c5d69de8536472e797
Cesar Barraza <cbarrazaa1@gmail.com>
2019-03-06 11:59:32 -0600

    Created bullet class

---
+++ src/examen2spaceinvaders/Bullet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examen2spaceinvaders;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author Cesar Barraza
+ */
+public class Bullet extends Item {
+    int speed = -5;
+    
+    public Bullet(int x, int y, int width, int height, int speed) {
+        super(x, y, width, height);
+        this.speed = speed;
+    }
+    
+    @Override
+    public void update() {
+        setY(getY() + speed);
+    }
+    
+    @Override
+    public void render(Graphics g) {
+        BufferedImage img = (speed > 0 ? Assets.enemyBullet : Assets.playerBullet);
+        g.drawImage(img, getX(), getY(), getWidth(), getHeight(), null);
+    }
+}
